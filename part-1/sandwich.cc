19af29bceee310cedc770e9c9ed743908084ba7f
Mohammed Omar <momar05@csu.fullerton.edu>
2023-11-01 22:20:28 -0700

    completed part 1 and 2

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -1,23 +1,25 @@
-// TODO: Add the required header
+// Mohammed Omar
+// momar05@csu.fullerton.edu
+// @momaarr27
+// Partners: @
+//
 
 #include <iostream>
 #include <string>
 #include <vector>
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> arguments{argv, argv + argc};
-
-  // TODO: Validate that the number of arguments is correct.
-  // If not, print an error message and return a non-zero value.
-
-  // TODO: Declare three std::string variables to hold the
-  // protein, bread, and condiment input.
-  // Initialize each variable with an element of the arguments vector
-  // declared above.
-
-  // TODO: Use cout to print output following the pattern
-  // Your order: A *PROTEIN* sandwich on *BREAD* with *CONDIMENT*.
-  // on its own line.
+  std::vector<std::string> arguments(argv, argv + argc);
+  if (arguments.size() != 4) {
+    std::cerr << "error: you must supply three arguments" << std::endl;
+    return 1;
+  }
+  std::string protein = arguments[1];
+  std::string bread = arguments[2];
+  std::string condiment = arguments[3];
+  std::cout << "Your order:" << std::endl;
+  std::cout << "A " << protein << " sandwich on " << bread << " with "
+            << condiment << "." << std::endl;
 
   return 0;
 }
--- part-2/average.cc
+++ part-2/average.cc
@@ -1,4 +1,8 @@
-// TODO: Add the required header
+// Mohammed Omar
+// momar05@csu.fullerton.edu
+// @momaarr27
+// Partners: @
+//
 
 #include <iostream>
 #include <string>
@@ -6,26 +10,28 @@
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
+  if (arguments.size() <= 1) {
+    std::cerr << "error: you must supply at least one number" << std::endl;
+    return 1;
+  }
 
-  // TODO: Validate that there is at least one command line argument.
-  // If not, print an error message and return a non-zero value.
+  double sum = 0.0;
 
-  // TODO: Write a for-each loop to sum (add up) all of the command line
-  // arguments.
-  // Use a double or float type so that your program preserves fractional
-  // values.
-  // The loop needs to skip over the command name, which is the first element
-  // of the arguments vector.
-  // Each argument is a std::string. You will need to convert each string into
-  // a number with the std::stod or std::stof function.
-
-  // TODO: After the loop has finished summing the arguments, calculate the
-  // average of the values. Recall that the average is the total value divided
-  // by the number of values.
-
-  // TODO: Use cout to print out a message of the form
-  // average = *AVERAGE*
-  // on its own line.
+  for (int i = 1; i < argc; ++i) {
+    try {
+      double value = std::stod(arguments[i]);
+      sum += value;
+    } catch (const std::invalid_argument& e) {
+      std::cerr << "error: invalid argument - " << arguments[i] << std::endl;
+      return 1;
+    } catch (const std::out_of_range& e) {
+      std::cerr << "error: argument out of range - " << arguments[i]
+                << std::endl;
+      return 1;
+    }
+  }
+  double average = sum / static_cast<double>(arguments.size() - 1);
+  std::cout << "average = " << average << std::endl;
 
   return 0;
 }
